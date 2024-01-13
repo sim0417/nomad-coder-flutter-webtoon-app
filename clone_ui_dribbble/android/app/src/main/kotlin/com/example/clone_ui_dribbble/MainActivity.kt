95d6c777b74cdfd124b97a3c8d933e2384bbd900
EonJo <sim04170@gmail.com>
2024-01-13 22:30:06 +0900

    init clone-ui project

---
+++ clone_ui_dribbble/android/app/src/main/kotlin/com/example/clone_ui_dribbble/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clone_ui_dribbble
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ clone_ui_dribbble/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
