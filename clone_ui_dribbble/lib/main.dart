62aa4bc91970e816d5dcba9fb427269663cd6bc2
EonJo <sim04170@gmail.com>
2024-01-13 23:33:57 +0900

    3.0 Header

--- clone_ui_dribbble/lib/main.dart
+++ clone_ui_dribbble/lib/main.dart
@@ -8,11 +8,42 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 40,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                  Text(
+                    'Hey, Selena',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  Text(
+                    'Welcome back',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
